375432c60b3391c79d3dcf4cad9b2c582535f8bb
Mark Rabey <mark@markrabey.com>
2022-04-26 15:35:10 -0400

    fix rename_project script

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-while getopts a:n:u:d flag
-do
+while getopts a:n:u:d flag; do
   case "${flag}" in
-    a) author=${OPTARG}
-    n) name=${OPTARG}
-    u) urlname=${OPTARG}
-    d) description=${OPTARG}
+  a) author=${OPTARG} ;;
+  n) name=${OPTARG} ;;
+  u) urlname=${OPTARG} ;;
+  d) description=${OPTARG} ;;
+  esac
 done
 
-echo "Author: $author";
-echo "Project Name: $name";
-echo "Project URL name: $urlname";
-echo "Description: $description";
+echo "Author: $author"
+echo "Project Name: $name"
+echo "Project URL name: $urlname"
+echo "Description: $description"
 
 echo "Renaming project..."
 
@@ -20,8 +20,7 @@ original_name="project_name"
 original_urlname="project_urlname"
 original_description="project_description"
 
-for filename in $(git ls-files)
-do
+for filename in $(git ls-files); do
   sed -i "s/$original_author/$author/g" $filename
   sed -i "s/$original_name/$name/g" $filename
   sed -i "s/$original_urlname/$urlname/g" $filename
